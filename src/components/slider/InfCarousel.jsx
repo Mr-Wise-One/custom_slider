c29f02f39a4253c481e1e615057c179b8cbfa51c
Felixjenkins <103826482+Felixjenkins@users.noreply.github.com>
2023-08-28 19:44:13 +0400

    modified:   src/components/slider/InfCarousel.jsx

--- src/components/slider/InfCarousel.jsx
+++ src/components/slider/InfCarousel.jsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 const InfCarousel = (props) => {
   const { step, infinite, children: cards } = props;
+  const [modifiedArr, setModifiedArr] = useState([]);
+  const [indexArr, setIndexArr] = useState([]);
+  const [arrInf, setArrInf] = useState([]);
 
   // const arr = React.Children.map(cards, (child, index) =>
   //   child.type.name === 'Card'
@@ -9,48 +12,54 @@ const InfCarousel = (props) => {
   //     : null
   // );
 
-  const arr = React.Children.map(cards, (child, index) =>
-    React.cloneElement(child, { cardIndex: index })
-  );
+  useEffect(() => {
+    const arr = React.Children.map(cards, (child, index) =>
+      React.cloneElement(child, { cardIndex: index })
+    );
 
-  const arrPart_1 = React.Children.map(arr, (child, index) =>
-    index < Math.ceil(arr.length / 2) && arr.length > 1
-      ? React.cloneElement(child, {
-          key: child.key + '-clone',
-          cardIndex: index + arr.length,
-        })
-      : arr.length === 1
-      ? React.cloneElement(child, {
-          key: child.key + '-clone-1',
-          cardIndex: index + arr.length,
-        })
-      : null
-  );
+    const arrPart_1 = React.Children.map(arr, (child, index) =>
+      index < Math.ceil(arr.length / 2) && arr.length > 1
+        ? React.cloneElement(child, {
+            key: child.key + '-clone',
+            cardIndex: index + arr.length,
+          })
+        : arr.length === 1
+        ? React.cloneElement(child, {
+            key: child.key + '-clone-1',
+            cardIndex: index + arr.length,
+          })
+        : null
+    );
 
-  const arrPart_2 = React.Children.map(arr, (child, index) =>
-    index >= Math.ceil(arr.length / 2) && arr.length > 1
-      ? React.cloneElement(child, {
-          key: child.key + '-clone',
-          cardIndex: index - arr.length,
-        })
-      : arr.length === 1
-      ? React.cloneElement(child, {
-          key: child.key + '-clone-2',
-          cardIndex: index - arr.length,
-        })
-      : null
-  );
+    const arrPart_2 = React.Children.map(arr, (child, index) =>
+      index >= Math.ceil(arr.length / 2) && arr.length > 1
+        ? React.cloneElement(child, {
+            key: child.key + '-clone',
+            cardIndex: index - arr.length,
+          })
+        : arr.length === 1
+        ? React.cloneElement(child, {
+            key: child.key + '-clone-2',
+            cardIndex: index - arr.length,
+          })
+        : null
+    );
 
-  const modifiedArr = arrPart_2.concat(arr, arrPart_1);
-  const indexArr = modifiedArr.map((child) => child.props.cardIndex);
+    setModifiedArr(arrPart_2.concat(arr, arrPart_1));
+    setIndexArr(modifiedArr.map((child) => child.props.cardIndex));
+  }, [cards]);
 
-  const arrInf = React.Children.map(modifiedArr, (child) =>
-    React.cloneElement(child, {
-      indexArr: indexArr,
-      step: step,
-      infinite: infinite,
-    })
-  );
+  useEffect(() => {
+    setArrInf(
+      React.Children.map(modifiedArr, (child) =>
+        React.cloneElement(child, {
+          indexArr: indexArr,
+          step: step,
+          infinite: infinite,
+        })
+      )
+    );
+  }, [step, cards, infinite]);
 
   return <div>{arrInf}</div>;
 };
